d09288832711e3331700f2b836d54e06d163cdec
Sameer Bhatt (debugger) <bhattsameer5@gmail.com>
2018-04-26 20:31:58 +0530

    Update xss_0.py

--- WAPT-SCANNER-2.0/xss_0.py
+++ WAPT-SCANNER-2.0/xss_0.py
@@ -92,16 +92,17 @@ try:
     	print "--------XSS Scanner--------"
     	print "\n"
     	parser = optparse.OptionParser(version=VERSION)
-    	parser.add_option("-u", "--url", dest="url", help="Target URL (e.g. \"http://www.target.com/page.php?id=1\")")
+    	#parser.add_option("-u", "--url", dest="url", help="Target URL (e.g. \"http://www.target.com/page.php?id=1\")")
     	parser.add_option("--data", dest="data", help="POST data (e.g. \"query=test\")")
     	parser.add_option("--cookie", dest="cookie", help="HTTP Cookie header value")
     	parser.add_option("--user-agent", dest="ua", help="HTTP User-Agent header value")
     	parser.add_option("--referer", dest="referer", help="HTTP Referer header value")
     	parser.add_option("--proxy", dest="proxy", help="HTTP proxy address (e.g. \"http://127.0.0.1:8080\")")
     	options, _ = parser.parse_args()
-    	if options.url:
+    	url = raw_input("Enter url with paramters: 	")
+    	if url:
         	init_options(options.proxy, options.cookie, options.ua, options.referer)
-        	result = scan_page(options.url if options.url.startswith("http") else "http://%s" % options.url, options.data)
+        	result = scan_page(url if url.startswith("http") else "http://%s" % url, options.data)
         	print "\nscan results: %s vulnerabilities found" % ("possible" if result else "no")
     	else:
         	parser.print_help()
